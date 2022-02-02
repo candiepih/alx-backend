071e97efbd6c5f40b040f3861d7273530c3004e0
Candiepih <44834632+candiepih@users.noreply.github.com>
2022-02-02 21:05:42 +0300

    Update 1-redis_op.js

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -4,15 +4,13 @@ import redis, {
 
 const client = createClient();
 
-(async () => {
-  client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
-  });
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
 
-  client.on('connect', () => {
-    console.log('Redis client connected to the server');
-  });
-})();
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
 
 const setNewSchool = (schoolName, value) => {
   client.set(schoolName, value, redis.print);
